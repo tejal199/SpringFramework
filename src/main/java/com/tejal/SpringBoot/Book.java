b8de8149641d365cdafd9824153b14808b712f2e
tejal199 <tejal1999jagtap@gmail.com>
2022-03-14 07:51:08 +0530

    Project

---
+++ src/main/java/com/tejal/SpringBoot/Book.java
@@ -0,0 +1,24 @@
+package com.tejal.SpringBoot;
+
+public class Book {
+	long id;
+	String name;
+	String author;
+	public Book(long id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+	public long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	
+	
+}
